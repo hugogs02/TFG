1b8d11b7e0b453da9e34a306c5b153576456d6c0
Hugo Gómez Sabucedo <hugogomezsabucedo@gmail.com>
2024-01-14 23:56:38 +0100

    cambio en longitud maxima de respuesta y en prueba.py

--- dataDownload.py
+++ dataDownload.py
@@ -30,8 +30,9 @@ def obtenArquivos(latW, latE, latS, latN, inicio, fin, parametro, directorioDesc
     aoi= "POLYGON((12.655118166047592 47.44667197521409,21.39065656328509 48.347694733853245,28.334291357162826 41.877123516783655,17.47086198383573 40.35854475076158,12.655118166047592 47.44667197521409))"
 
     # make the request
-    jsonf=requests.get(f"https://catalogue.dataspace.copernicus.eu/odata/v1/Products?$filter=OData.CSC.Intersects(area=geography'SRID=4326;{aoi}') and Collection/Name eq 'SENTINEL-5P' and contains(Name,'S5P_OFFL_{parametro}') and ContentDate/Start gt {inicio}T00:00:00.000Z and ContentDate/Start lt {fin}T00:00:00.000Z").json()
+    jsonf=requests.get(f"https://catalogue.dataspace.copernicus.eu/odata/v1/Products?$filter=OData.CSC.Intersects(area=geography'SRID=4326;{aoi}') and Collection/Name eq 'SENTINEL-5P' and contains(Name,'S5P_OFFL_{parametro}') and ContentDate/Start gt {inicio}T00:00:00.000Z and ContentDate/Start lt {fin}T00:00:00.000Z&$top=100").json()
     df = pd.DataFrame.from_dict(jsonf['value'])
+
     print(f"\nDescargaranse {len(df)} arquivos.")
 
     keycloak_token = get_keycloak('hugo.gomez.sabucedo@rai.usc.es', 'Hugotfg&2023')
@@ -42,6 +43,8 @@ def obtenArquivos(latW, latE, latS, latN, inicio, fin, parametro, directorioDesc
     session.headers.update(headers)
 
     download_dir = directorioDescarga
+    if not os.path.exists(directorioDescarga):
+        os.mkdir(directorioDescarga)
 
     for i in range(len(df)):
         pr = df.Id.values[i]
