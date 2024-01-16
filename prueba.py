f332f21a66a572ebd82ff7dbd8fdf0f831a84ef6
Hugo Gómez Sabucedo <hugogomezsabucedo@gmail.com>
2024-01-16 23:27:31 +0100

    cambio en forma de descarga, descompresion en zip y display de mensajes de error

--- prueba.py
+++ prueba.py
@@ -1,7 +1,7 @@
 import app
 
-"""app.run("L2__NO2___", "2023-11-01", "2023-11-30", "data/unproc/", "data/NO2/")
+app.run("L2__NO2___", "2023-11-01", "2023-11-30", "data/unproc/", "data/NO2/")
 app.run("L2__CO____", "2023-11-01", "2023-11-30", "data/unproc/", "data/CO/")
 app.run("L2__O3____", "2023-11-01", "2023-11-30", "data/unproc/", "data/O3/")
-app.run("L2__SO2___", "2023-11-01", "2023-11-30", "data/unproc/", "data/SO2/")"""
+app.run("L2__SO2___", "2023-11-01", "2023-11-30", "data/unproc/", "data/SO2/")
 app.run("L2__CH4___", "2023-11-01", "2023-11-02", "data/unproc/", "data/CH4/")
