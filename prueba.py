6794deb2b4bc86ecb3be7651fc474e2bca9b9aa8
Hugo Gómez Sabucedo <hugogomezsabucedo@gmail.com>
2024-01-18 23:33:18 +0100

    cambio en aoi

--- prueba.py
+++ prueba.py
@@ -2,6 +2,6 @@ import app
 
 app.run("L2__NO2___", "2023-11-01", "2023-11-30", "data/unproc/", "data/NO2/")
 app.run("L2__CO____", "2023-11-01", "2023-11-30", "data/unproc/", "data/CO/")
-"""app.run("L2__O3____", "2023-11-01", "2023-11-30", "data/unproc/", "data/O3/")
+app.run("L2__O3____", "2023-11-01", "2023-11-30", "data/unproc/", "data/O3/")
 app.run("L2__SO2___", "2023-11-01", "2023-11-30", "data/unproc/", "data/SO2/")
-app.run("L2__CH4___", "2023-11-01", "2023-11-30", "data/unproc/", "data/CH4/")"""
+app.run("L2__CH4___", "2023-11-01", "2023-11-30", "data/unproc/", "data/CH4/")
